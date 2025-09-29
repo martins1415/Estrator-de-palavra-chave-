d5529858b9a96f9cdf7f1c472f711c4e55df79be
martins1415 <luciano.jose.martins@escola.pr.gov.br>
2025-09-29 09:03:00 -0300

    ctgh

--- script.js
+++ script.js
@@ -13,17 +13,21 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
-    let frequencias = {};
-    for (let i of palavras) {
-        frequencias[i] = 0;
-        for (let j of palavras) {
-            if (i == j) {
-                frequencias[i]++;
-            }
-        }
-    }
+   contraFrequencias(palavras);
+   
+   let frequencias = {};
+   for (let i of palavras) {
+       frequencias[i] = 0;
+       for (let j of palavras) {
+           if (i == j) {
+               frequencias[i]++;
+           }
+       }
+   }
 
-    console.log(frequencias);
 
+
+
+   console.log(frequencias);
     return palavras;
 }
